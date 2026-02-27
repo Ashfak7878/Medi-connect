e515c57a10e836f8c8e9fe7743a21cdc75ec9da5
Ashfak Nasar <ashfaknasar7878@gmail.com>
2026-02-27 12:11:41 +0530

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { registerController, loginController } = require('../controllers/userCtrl'); // Added loginController here
+const { registerController, loginController, getUserDataController } = require('../controllers/userCtrl');
+
+// Bring in the Bouncer!
+const authMiddleware = require('../middlewares/authMiddleware'); 
 
 const router = express.Router();
 
-// REGISTER || POST
+// Public Routes (Anyone can access these without a wristband)
 router.post('/register', registerController);
+router.post('/login', loginController);
 
-// LOGIN || POST
-router.post('/login', loginController); // The new menu item
+// VIP Protected Route (You MUST pass the authMiddleware to reach the controller)
+router.post('/getUserData', authMiddleware, getUserDataController); 
 
 module.exports = router;
